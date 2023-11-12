277f0b505f0738b2d5d18bf51398f849bb6bbd3c
testingdreambooth2 <149936928+testingdreambooth2@users.noreply.github.com>
2023-11-12 12:32:48 +0100

    Update server.js
    hope

--- server.js
+++ server.js
@@ -17,6 +17,6 @@ app.post('/make-api-request', async (req, res) => {
     }
 });
 
-//app.listen(port, () => {
-//    console.log(`Server is running on port ${port}`);
-//});
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
