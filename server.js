afb22839b9e1cf5771b504211ab2fd36b9ceebad
testingdreambooth2 <149936928+testingdreambooth2@users.noreply.github.com>
2023-11-12 11:58:10 +0100

    Update server.js
    changed ports

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const axios = require('axios');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(express.json());
 
