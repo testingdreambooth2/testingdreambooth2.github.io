57e98e7dc65310dfa13aec2134b515b2c49842e3
testingdreambooth2 <testingdreambooth2@gmail.com>
2023-12-09 01:45:49 +0100

    n

---
+++ functions/make-api-request.js
@@ -0,0 +1,24 @@
+// functions/make-api-request.js
+const axios = require('axios');
+
+exports.handler = async function (event, context) {
+  try {
+    const response = await axios.post('https://users.roblox.com/v1/usernames/users', JSON.parse(event.body));
+
+    return {
+      statusCode: 200,
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'POST',
+        'Access-Control-Allow-Headers': 'Content-Type',
+      },
+      body: JSON.stringify(response.data),
+    };
+  } catch (error) {
+    console.error(error);
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: 'Internal Server Error' }),
+    };
+  }
+};
