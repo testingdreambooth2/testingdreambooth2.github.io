8290c692d7ef421eb05e30ed789eed481158c79c
testingdreambooth2 <149936928+testingdreambooth2@users.noreply.github.com>
2024-03-06 19:51:02 +0100

    Update getExperienceId.js

--- functions/getExperienceId.js
+++ functions/getExperienceId.js
@@ -12,7 +12,10 @@ exports.handler = async function (event, context) {
     };
   }
 
-  // Define CORS headers
+  
+  // Define CORS headers https://rbxhandeltest.myshopify.com
+  // Original 'Access-Control-Allow-Origin': 'https://robloxhandeltester.myshopify.com', 
+  // Test-side 'Access-Control-Allow-Origin': 'https://rbxhandeltest.myshopify.com/', 
   const headers = {
     'Access-Control-Allow-Origin': 'https://robloxhandeltester.myshopify.com',
     'Access-Control-Allow-Origin':  'https://rbxhandeltest.myshopify.com',
