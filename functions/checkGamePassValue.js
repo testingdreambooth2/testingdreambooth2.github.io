0b76bb704ca15907ee51854f6168c9528735d356
testingdreambooth2 <149936928+testingdreambooth2@users.noreply.github.com>
2024-03-06 19:47:37 +0100

    Update checkGamePassValue.js

--- functions/checkGamePassValue.js
+++ functions/checkGamePassValue.js
@@ -17,7 +17,7 @@ exports.handler = async function (event, context) {
   // Define CORS headers
   const headers = {
      'Access-Control-Allow-Origin': 'https://robloxhandeltester.myshopify.com, 
-     'Access-Control-Allow-Origin':  'https://rbxhandeltest.myshopify.com',
+//     'Access-Control-Allow-Origin':  'https://rbxhandeltest.myshopify.com',
     'Access-Control-Allow-Methods': 'GET',
     'Access-Control-Allow-Headers': 'Content-Type',
     'Access-Control-Allow-Credentials': 'true',
