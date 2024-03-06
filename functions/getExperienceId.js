3c41830ce80e28d88d7698dbb1a13230789ad27f
testingdreambooth2 <149936928+testingdreambooth2@users.noreply.github.com>
2024-03-06 19:56:11 +0100

    Update getExperienceId.js

--- functions/getExperienceId.js
+++ functions/getExperienceId.js
@@ -15,7 +15,7 @@ exports.handler = async function (event, context) {
   
   // Define CORS headers https://rbxhandeltest.myshopify.com
   // Original 'Access-Control-Allow-Origin': 'https://robloxhandeltester.myshopify.com', 
-  // Test-side 'Access-Control-Allow-Origin': 'https://rbxhandeltest.myshopify.com/', 
+  // Test-side 'Access-Control-Allow-Origin': 'https://rbxhandeltest.myshopify.com', 
   const headers = {
     'Access-Control-Allow-Origin':  'https://rbxhandeltest.myshopify.com',
     'Access-Control-Allow-Methods': 'GET',
