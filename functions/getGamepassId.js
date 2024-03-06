c3c40c5479f4648b6c3db6199d6f53b7d587da55
testingdreambooth2 <149936928+testingdreambooth2@users.noreply.github.com>
2024-03-06 19:51:14 +0100

    Update getGamepassId.js

--- functions/getGamepassId.js
+++ functions/getGamepassId.js
@@ -12,7 +12,9 @@ exports.handler = async function (event, context) {
     };
   }
 
-  // Define CORS headers
+   // Define CORS headers https://rbxhandeltest.myshopify.com
+  // Original 'Access-Control-Allow-Origin': 'https://robloxhandeltester.myshopify.com', 
+  // Test-side 'Access-Control-Allow-Origin': 'https://rbxhandeltest.myshopify.com/', 
   const headers = {
      'Access-Control-Allow-Origin': 'https://robloxhandeltester.myshopify.com, https://rbxhandeltest.myshopify.com',
     'Access-Control-Allow-Methods': 'GET',
