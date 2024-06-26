36e41315ca58e2416c15092ea4984344169b0f2b
testingdreambooth2 <149936928+testingdreambooth2@users.noreply.github.com>
2024-06-26 19:43:16 +0200

    Update login.js

--- functions/login.js
+++ functions/login.js
@@ -2,7 +2,7 @@ const axios = require('axios');
 
 exports.handler = async function (event, context) {
   const headers = {
-    'Access-Control-Allow-Origin': 'https://xn--kbrbx-vua.dk',
+    'Access-Control-Allow-Origin': 'https://robloxhandeltester.myshopify.com',
     'Access-Control-Allow-Methods': 'POST, OPTIONS',
     'Access-Control-Allow-Headers': 'Content-Type',
     'Access-Control-Allow-Credentials': 'true',
@@ -23,7 +23,7 @@ exports.handler = async function (event, context) {
 
     const clientId = "7886324157267699256";
     const clientSecret = "RBX-74nJstuoZ0mWhQtPVwX5hlhOWaM3mRs23be_nXual-yZRSCN8d2w3UYxmUnwKVIY";
-    const redirectUri = 'https://xn--kbrbx-vua.dk/callback';
+    const redirectUri = 'https://robloxhandeltester.myshopify.com/callback';
 
     const data = new URLSearchParams();
     data.append('client_id', clientId);
